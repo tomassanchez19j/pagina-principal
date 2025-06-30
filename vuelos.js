338504c393e79c037e7ed70d90a4e2dd107ac85c
tomassanchez19j <tomassanxhez91@gmail.com>
2025-06-30 05:13:46 -0300

    hice de todo

--- vuelos.js
+++ vuelos.js
@@ -1,6 +1,128 @@
+import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm"
+
+
+const supabaseUrl = "https://gtggrdzrgifuqoihvjll.supabase.co"
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imd0Z2dyZHpyZ2lmdXFvaWh2amxsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDk3NTY4MjAsImV4cCI6MjA2NTMzMjgyMH0.vvhAkTFCsJYbd0W_5tYrwsWtQMjeXnqjB4ybC3zEN-k"
+const supabase = createClient(supabaseUrl, supabaseKey)
+const btn_comprar = document.querySelector(".btn_comprar")
+const elemento = document.querySelector(".container")
+const url = "https://api-viajes-77bq.vercel.app/api/vuelos/leer"
 const formulario_compra_simple = document.querySelector(".formulario-compra-simple")
 const compra = document.querySelector(".compra_flotante")
 const compra_simple = document.querySelector(".compra_contenido")
+
+
+
+fetch(url)
+  .then(res => res.json())
+  .then(data => {
+    data.forEach(vuelo => {
+      const card = document.createElement("div")
+      card.classList.add("card-vuelo", "extra")
+      card.id = vuelo.id_vuelos
+      card.innerHTML = `
+        
+          <div class="vuelo-top" ">
+            <img src="img/logo.png" alt="Logo Aerolínea">
+            <span>jet & go</span>
+          </div>
+          <div class="vuelo-body">
+            <p>PARTIDA</p>
+            <p>${vuelo.partida} → ${vuelo.destino}</p>
+            <p>${vuelo.fecha_de_salida} · ${vuelo.hora_de_salida}</p>
+            <p>LLEGADA</p>
+            <p>${vuelo.partida} → ${vuelo.destino}</p>
+            <p>${vuelo.fecha_de_salida} · ${vuelo.hora_de_salida}</p>
+          </div>
+          <div class="vuelo-bottom">
+            <h4>$${vuelo.precio}</h4>
+          </div>
+          <section class="vuelo-botones">
+            <a href="#" class="btn_comprar">comprar</a>
+            <a href="#" class="btn_agregar_carrito">+</a>
+          </section>
+        
+      `
+      elemento.appendChild(card)
+    })
+  })
+    
+
+    
+    
+
+
+
+ elemento.addEventListener("click", (e) => {
+  if(e.target.classList.contains("btn_comprar")){
+    compra.style.display = "flex"
+  }
+  })
+
+    
+compra.addEventListener("click", (e) => {
+    if (!e.target.closest(".compra_contenido")) {
+      compra.style.display = "none"
+    }
+})
+
+function calcularTotal() {
+    let total = 0
+    document.querySelectorAll(".carrito-contenido h4").forEach(el => {
+      total += Number(el.textContent.replace("$", ""))
+    })
+    document.getElementById("total-carrito").textContent = `$${total}`
+  }
+
+const contenido_carrito = document.querySelector(".carrito-contenido")
+const btn_carrito = document.getElementById("carrito")
+const carrito = document.querySelector(".carrito-flotante")
+ btn_carrito.addEventListener("click", (e) => {
+  if(e.target.classList.contains("btn")){
+    carrito.style.display = "flex"
+   
+  }
+  })
+carrito.addEventListener("click", (e) => {
+    if (!e.target.closest(".carrito-contenido")) {
+      carrito.style.display = "none"
+    }
+})
+
+
+
+
+
+const div2 =document.createElement("div")
+elemento.addEventListener("click", (e) => {
+  if (e.target.classList.contains("btn_agregar_carrito")) {
+   
+    const card = e.target.closest(".card-vuelo")
+    if (card) {
+
+
+      
+      const vueloHTML = card.cloneNode(true)
+      const btn_basura = vueloHTML.querySelector(".btn_agregar_carrito")
+      const btn_eliminar_carrito = vueloHTML.querySelector(".btn_comprar")
+      
+      btn_basura.remove()
+      btn_eliminar_carrito.textContent = ("-")
+      vueloHTML.classList.remove("card-vuelo")
+      vueloHTML.classList.add("card-vuelo2")
+      contenido_carrito.appendChild(vueloHTML)
+      calcularTotal()
+    }
+  } else {
+    console.log("err")
+  }
+})
+
+
+
+
+
+
 
 // Muestra el formulario de compra y guarda los datos del vuelo en dataset
 compra.addEventListener("click", (e) => {
